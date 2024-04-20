86aa9355bb69e7fb2782f9e195112aafc6fc2cb5
Daví Marques <50294449+marquesdavi@users.noreply.github.com>
2024-04-20 14:36:47 -0300

    add basic project structure

---
+++ src/main/java/com/api/manager/fleet/FleetIoApplication.java
@@ -0,0 +1,13 @@
+package com.api.manager.fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/manager/fleet/FleetIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.manager.fleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
